27bcde1b44d223995677e57f2d23d60187993771
Vedant Lohbare <vedant.lohbare@gmail.com>
2020-11-08 15:16:40 +0530

    Created the structure for the ChatWidget #10

---
+++ src/components/ChatWidget/ChatWidget.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+import { colors } from "../../styles/GlobalStyles"
+
+export const Widget = styled.div`
+	position: fixed;
+	bottom: 50px;
+	right: 50px;
+	z-index: 99;
+	font-size: 18px;
+	border: none;
+	outline: none;
+	background-color: white;
+	cursor: pointer;
+	padding: 10px;
+	opacity: 0.7;
+	border-radius: 4px;
+`
---
+++ src/components/ChatWidget/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Card } from "react-bootstrap"
+import { Widget } from "./ChatWidget.styles"
+const ChatWidget = () => (
+	<Widget>
+		<h1>Vedant</h1>
+		<p>Join Chat</p>
+	</Widget>
+)
+
+export default ChatWidget
